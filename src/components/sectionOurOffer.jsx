3e005ae5a1cb1b5631e13970027948ad666d704b
Wiktor <lolxdyoloelo@gmail.com>
2024-05-04 19:54:54 +0200

    section customer testimonials

--- src/components/sectionOurOffer.jsx
+++ src/components/sectionOurOffer.jsx
@@ -2,38 +2,38 @@ import image from "../assets/images/telebim_background.jpg";
 import navigate from "../assets/icons/navigate.svg";
 
 const OfferCard = (props) => {
-    return (
-        <>
-            <div className="offerCard">
-                <div className="stylizedBox">
-                    <div className="offerImage"></div>
-                </div>
+  return (
+    <>
+      <div className="offerCard">
+        <div className="stylizedBox">
+          <div className="offerImage"></div>
+        </div>
 
-                <div className="offerHyperlink">
-                    <div className="reachUsContactIconBack">
-                        <img src={navigate} className="reachUsContactIcon" />
-                    </div>
-                    <span>{props.offerText}</span>
-                </div>
-            </div>
-        </>
-    )
-}
+        <div className="offerHyperlink">
+          <div className="reachUsContactIconBack">
+            <img src={navigate} className="reachUsContactIcon" />
+          </div>
+          <span>{props.offerText}</span>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default function SectionOurOffer(props) {
-    return (
-        <>
-            <div className="leftSide">
-                <OfferCard offerText="Tyre replacements" />
-                <OfferCard offerText="Tyre replacements" />
-                <OfferCard offerText="Tyre replacements" />
-            </div>
+export default function SectionOurOffer() {
+  return (
+    <>
+      <div className="leftSide">
+        <OfferCard offerText="Tyre replacements" />
+        <OfferCard offerText="Tyre replacements" />
+        <OfferCard offerText="Tyre replacements" />
+      </div>
 
-            <div className="rightSide">
-                <OfferCard offerText="Tyre replacements" />
-                <OfferCard offerText="Tyre replacements" />
-                <OfferCard offerText="Tyre replacements" />
-            </div>
-        </>
-    )
+      <div className="rightSide">
+        <OfferCard offerText="Tyre replacements" />
+        <OfferCard offerText="Tyre replacements" />
+        <OfferCard offerText="Tyre replacements" />
+      </div>
+    </>
+  );
 }
--- src/components/sectionTestimonials.jsx
+++ src/components/sectionTestimonials.jsx
@@ -1,7 +1,71 @@
-export default function SectionTestimonials(props) {
+import quote from "../assets/images/quote.png";
+import star from "../assets/images/star.svg";
+
+export default function SectionTestimonials() {
   return (
     <>
-      <div className="formContainer stylizedBox">abc</div>
+      <div className="testimonials-box">
+        <div className="stylizedBox testimonial">
+          <img className="quote" src={quote} />
+          <div className="testimonial-name">
+            <span>Ian “Haggis” Gatens</span>
+          </div>
+          <div className="testimonial-stars-bar">
+            <img src={star} className="star" />
+            <img src={star} className="star" />
+            <img src={star} className="star" />
+            <img src={star} className="star" />
+            <img src={star} className="star" />
+          </div>
+          <div className="testimonial-content">
+            Good prices, solution orientated, friendly, great service. Got me
+            out of a pickle and on my way home same day. One phone call, short
+            notice supply and fit of two tyres to make my towing rig safe &
+            sound. Highly recommended. Thank you.
+          </div>
+          <div className="testimonial-source">Source: Google Maps</div>
+        </div>
+        <div className="stylizedBox testimonial">
+          <img className="quote" src={quote} />
+          <div className="testimonial-name">
+            <span>Ian “Haggis” Gatens</span>
+          </div>
+          <div className="testimonial-stars-bar">
+            <img src={star} className="star" />
+            <img src={star} className="star" />
+            <img src={star} className="star" />
+            <img src={star} className="star" />
+            <img src={star} className="star" />
+          </div>
+          <div className="testimonial-content">
+            Good prices, solution orientated, friendly, great service. Got me
+            out of a pickle and on my way home same day. One phone call, short
+            notice supply and fit of two tyres to make my towing rig safe &
+            sound. Highly recommended. Thank you.
+          </div>
+          <div className="testimonial-source">Source: Google Maps</div>
+        </div>
+        <div className="stylizedBox testimonial">
+          <img className="quote" src={quote} />
+          <div className="testimonial-name">
+            <span>Ian “Haggis” Gatens</span>
+          </div>
+          <div className="testimonial-stars-bar">
+            <img src={star} className="star" />
+            <img src={star} className="star" />
+            <img src={star} className="star" />
+            <img src={star} className="star" />
+            <img src={star} className="star" />
+          </div>
+          <div className="testimonial-content">
+            Good prices, solution orientated, friendly, great service. Got me
+            out of a pickle and on my way home same day. One phone call, short
+            notice supply and fit of two tyres to make my towing rig safe &
+            sound. Highly recommended. Thank you.
+          </div>
+          <div className="testimonial-source">Source: Google Maps</div>
+        </div>
+      </div>
     </>
   );
 }
