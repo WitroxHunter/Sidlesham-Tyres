1f390d5fdcb4df93e5baf3945db584b45b99008f
KubixekTV <jakub.mazur11@hotmail.com>
2024-05-06 18:21:10 +0200

    commicior

--- src/components/sectionWhyUs.jsx
+++ src/components/sectionWhyUs.jsx
@@ -18,10 +18,10 @@ const WhyUsFeature = (props) => {
 export default function SectionWhyUs(props) {
     return (
         <>
-            <div className="leftSide">
-                <WhyUsFeature feature="Expertise and Specialization:" text="Benefit from our specialized knowledge in high-performance and 4x4 tyres. Our skilled team offers tailored advice and service." />
-                <WhyUsFeature feature="Wide Range of Options:" text="Discover diverse tyre options for various vehicles, including vans. We provide top-quality tyres to ensure the perfect fit." />
-                <WhyUsFeature feature="Quality Assurance:" text="Rest assured, every tyre undergoes rigorous quality checks for durability and performance. We stock only reputable brands known for exceptional quality." />
+            <div className="leftSide ">
+                <WhyUsFeature feature="Expertise and Specialization" text="Benefit from our specialized knowledge in high-performance and 4x4 tyres. Our skilled team offers tailored advice and service." />
+                <WhyUsFeature feature="Wide Range of Options" text="Discover diverse tyre options for various vehicles, including vans. We provide top-quality tyres to ensure the perfect fit." />
+                <WhyUsFeature feature="Quality Assurance" text="Rest assured, every tyre undergoes rigorous quality checks for durability and performance. We stock only reputable brands known for exceptional quality." />
             </div>
             <div className="rightSide">
                 <img src={tires}></img>
