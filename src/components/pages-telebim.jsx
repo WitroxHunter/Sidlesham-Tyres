13612b8ce3c79041249509927ca91c1a82386c50
KubixekTV <jakub.mazur11@hotmail.com>
2024-05-08 15:22:21 +0200

    Page "About us" - almost ready

--- src/components/pages-telebim.jsx
+++ src/components/pages-telebim.jsx
@@ -1,10 +1,12 @@
 export default function PagesTelebim(props) {
   return (
     <>
-      <div className={`pages-telebim stylizedBox ${props.background}`}>
-        <div className="pages-telebim-box">
-          <h1 className="colorUIorange">{props.sectionName}</h1>
-          <span className="fontSizeMid">{props.text}</span>
+      <div className={`pages-telebim-background stylizedBoxNoBG ${props.background}`}>
+        <div className={`pages-telebim`}>
+          <div className="pages-telebim-box">
+            <h1 className="colorUIorange">{props.sectionName}</h1>
+            <div className="fontSizeMid">{props.text}</div>
+          </div>
         </div>
       </div>
     </>
--- src/pages/about-us.jsx
+++ src/pages/about-us.jsx
@@ -1,6 +1,11 @@
 import Header from "../components/header";
 import PagesTelebim from "../components/pages-telebim";
+import TireMarks from "../components/splitterTireMarks";
 import Footer from "../components/footer";
+import image1 from "../assets/images/Screenshot_gd_porsche_911_turbo_3_0_ks_nordschleife_5-4-124-21-11-25.jpg";
+import image2 from "../assets/images/Screenshot_180sx_mission_2_drift_10-3-124-21-40-48.jpg";
+import image3 from "../assets/images/Screenshot_a3dr_ferrari_512tr_ks_brands_hatch_4-4-124-18-24-52.jpg";
+import image4 from "../assets/images/Screenshot_yk_subaru_sedan_sti_ks_vallelunga_5-4-124-22-10-9.jpg";
 import "./about-us.css";
 
 function Subline(props) {
@@ -15,6 +20,21 @@ function Subline(props) {
   );
 }
 
+const TextBox = (props) => {
+  return (
+    <>
+      <div className={`article-flex-box article-flex-${props.direction}`}>
+        <div className="article-text">
+          {props.text}
+        </div>
+        <div className="article-photo-box">
+          <img src={props.photo} className="article-photo stylizedBoxNoBG" />
+        </div>
+      </div>
+    </>
+  )
+}
+
 export default function AboutUs() {
   return (
     <>
@@ -25,10 +45,34 @@ export default function AboutUs() {
         background="about-us-background"
       />
 
+      <TireMarks />
+
       <div className="main-center">
+        <Subline title="Our mission" />
+        <TextBox
+          direction="forward"
+          photo={image1}
+          text="At Sidlesham Tyres, our mission is simple: to provide expert fitting and good prices in a fresh and clean environment. We believe in honesty and integrity, which is why we follow the philosophy of only selling you what you need."
+        />
+
         <Subline title="Our story" />
-        <Subline title="Our story" />
-        <Subline title="Our story" />
+        <TextBox
+          direction="reverse"
+          photo={image2}
+          text="Established in April 2015, Sidlesham Tyres was born out of a desire to offer superior service and value to our customers. After years of experience working for another local tyre fitting firm, our founder decided it was time to strike out on their own. With a background in tyre fitting since 1999, our commitment to quality and customer satisfaction has remained unwavering."
+        />
+        <TextBox
+          direction="forward"
+          photo={image3}
+          text="Established in April 2015, Sidlesham Tyres was born out of a desire to offer superior service and value to our customers. After years of experience working for another local tyre fitting firm, our founder decided it was time to strike out on their own. With a background in tyre fitting since 1999, our commitment to quality and customer satisfaction has remained unwavering."
+        />
+
+        <Subline title="Our services" />
+        <TextBox
+          direction="reverse"
+          photo={image4}
+          text="Sidlesham Tyres is your one-stop shop for all your tyre needs. We supply tyres for cars, vans, 4x4s, trailers, caravans, and motor-homes, ranging from budget options to top brands like Michelin. But that's not all—we also stock mower tyres, wheelbarrow tyres, and mobility scooter tyres. If your tyres hold air, chances are we can supply them at bargain prices."
+        />
       </div>
       <Footer />
     </>
