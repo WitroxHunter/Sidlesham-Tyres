80a0b4ae18af25b3f6f6af873adaa3d1f921e7b7
KubixekTV <73798453+KubixekTv@users.noreply.github.com>
2024-06-20 19:14:59 +0200

    Modified telebim on pages

--- src/components/pages-telebim.jsx
+++ src/components/pages-telebim.jsx
@@ -3,8 +3,9 @@ import AnimatedComponent from "./Reveal";
 export default function PagesTelebim(props) {
   return (
     <>
-      <div className={`pages-telebim-background stylizedBoxNoBG ${props.background}`}>
-        <div className={`pages-telebim`}>
+      <div className={`pages-telebim-background ${props.background}`}>
+        <div className="pages-telebim">
+          <div className="pages-telebim-darkening" />
           <div className="pages-telebim-box">
             <AnimatedComponent>
               <h1 className="colorUIorange">{props.sectionName}</h1>
