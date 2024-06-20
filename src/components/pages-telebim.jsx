c273e733e4c5d6b0e8d8ccd7636049c7fbd73703
KubixekTV <73798453+KubixekTv@users.noreply.github.com>
2024-06-20 23:07:39 +0200

    Added simple scroll down button on sub pages

--- src/components/pages-telebim.jsx
+++ src/components/pages-telebim.jsx
@@ -1,10 +1,40 @@
+import { useState, useEffect } from "react";
 import AnimatedComponent from "./Reveal";
+import arrowDown from "../assets/icons/arrow_down.svg";
 
 export default function PagesTelebim(props) {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 0;
+      setScrolled(isScrolled);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const handleClick = () => {
+    window.scrollTo({
+      top: window.innerHeight,
+      behavior: "smooth",
+    });
+  };
+
   return (
     <>
       <div className={`pages-telebim-background ${props.background}`}>
         <div className="pages-telebim">
+          <div
+            className={
+              scrolled ? "scroll-down-button-scrolled" : "scroll-down-button"
+            }
+          >
+            <img src={arrowDown} onClick={handleClick} />
+          </div>
           <div className="pages-telebim-darkening" />
           <div className="pages-telebim-box">
             <AnimatedComponent>
