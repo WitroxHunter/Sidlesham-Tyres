78e5cee9901b74adc1b23855ea5364cbd32acc82
Wiktor <lolxdyoloelo@gmail.com>
2024-06-20 15:15:08 +0200

    Captcha dziala

--- src/components/sectionGetAQuote.jsx
+++ src/components/sectionGetAQuote.jsx
@@ -1,14 +1,21 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
+import ReCAPTCHA from "react-google-recaptcha";
 
 export default function SectionGetAQuote(props) {
   const form = useRef();
+  const [capVal, setCapVal] = useState(null);
 
   const sendEmail = async (e) => {
     e.preventDefault();
 
+    if (!capVal) {
+      alert("Please complete the CAPTCHA");
+      return;
+    }
+
     const formData = new FormData(form.current);
     const formObject = Object.fromEntries(formData.entries());
-    console.log(formData);
+    formObject.captchaToken = capVal; // Add the captcha value to the form data
 
     try {
       const response = await fetch("http://localhost:5000/send", {
@@ -40,36 +47,42 @@ export default function SectionGetAQuote(props) {
             <div className="leftSide">
               <div className="formInput">
                 <label>Name</label>
-                <input type="text" name="name"></input>
+                <input type="text" name="name" required></input>
               </div>
 
               <div className="formInput">
                 <label>E-mail</label>
-                <input type="text" name="email"></input>
+                <input type="email" name="email" required></input>
               </div>
             </div>
 
             <div className="rightSide">
               <div className="formInput">
                 <label>Phone number</label>
-                <input type="text" name="phoneNumber"></input>
+                <input type="text" name="phoneNumber" required></input>
               </div>
 
               <div className="formInput">
                 <label>Vehicle type</label>
-                <input type="text" name="vehicleType"></input>
+                <input type="text" name="vehicleType" required></input>
               </div>
             </div>
           </div>
 
           <div className="formInput">
             <label>Service needed</label>
-            <textarea name="serviceNeeded"></textarea>
+            <textarea name="serviceNeeded" required></textarea>
           </div>
 
-          <button className="formButtonDiv">
+          <ReCAPTCHA
+            sitekey="6Ld4Wf0pAAAAAMkYUiUG_8Tb4xAofKOikiieekiV"
+            onChange={(val) => setCapVal(val)}
+          />
+
+          <button disabled={!capVal} className="formButtonDiv">
             <div>
               <input
+                disabled={!capVal}
                 className="formButton"
                 type="submit"
                 value={"Send report"}
