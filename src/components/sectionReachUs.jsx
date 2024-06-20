587d02b8abc12fb56f9487f3d994e96aed38e962
Wiktor <lolxdyoloelo@gmail.com>
2024-06-20 15:36:55 +0200

    Loading... on google maps

--- src/components/sectionReachUs.jsx
+++ src/components/sectionReachUs.jsx
@@ -20,6 +20,13 @@ export default function sectionReachUs() {
   useEffect(() => {
     setMessage(data.message);
   }, []);
+
+  const [loading, setLoading] = useState(true);
+
+  const handleIframeLoad = () => {
+    setLoading(false);
+  };
+
   return (
     <>
       <div className="verticalFlex">
@@ -51,6 +58,7 @@ export default function sectionReachUs() {
           </div>
 
           <div className="rightSide stylizedBox">
+            {loading && <div className="loading">Loading...</div>}
             <iframe
               className="googleMaps"
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2521.975918711559!2d-0.8002860231335861!3d50.79455486252758!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487453b0fc85be59%3A0x2fa5e8ff333cd02a!2sSidlesham%20Tyres!5e0!3m2!1spl!2spl!4v1714412972416!5m2!1spl!2spl"
@@ -59,6 +67,7 @@ export default function sectionReachUs() {
               loading="lazy"
               referrerPolicy="no-referrer-when-downgrade"
               title="Google Map"
+              onLoad={handleIframeLoad}
             ></iframe>
           </div>
         </div>
